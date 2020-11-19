ad93fd0d217a368eef170220649e519391c43aa3
Edouard Theron <ed@edtheron.me>
2020-11-18 19:14:06 -0600

    Update shinatra.sh
    Enable a 3rd argument to launch a command when endpoint is reached.

--- shinatra.sh
+++ shinatra.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 RESPONSE="HTTP/1.1 200 OK\r\nConnection: keep-alive\r\n\r\n${2:-"OK"}\r\n"
 while { echo -en "$RESPONSE"; } | nc -l "${1:-8080}"; do
+  if [[ $3 ]]; then
+    eval $3
+  fi
   echo "================================================"
 done
